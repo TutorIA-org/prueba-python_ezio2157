c90b0d13474cccb88e501c5894e7122c28a75cac
TutorIA-TFG <tutoria2025.tfg@gmail.com>
2025-07-10 13:15:23 +0200

    Reentrega - limpieza anterior

--- par_o_impar.py
+++
@@ -1,8 +0,0 @@
-# Solicita al usuario que ingrese un número entero
-numero = int(input("Introduce un número entero: "))
-
-# Determina si el número es par o impar
-if numero % 2 == 0:
-    print(f"El número {numero} es par.")
-else:
-    print(f"El número {numero} es impar.")
